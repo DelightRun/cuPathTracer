34b070b4a70026b730e29471dfb1c221cef0cf2b
王昌旭 <wang_changxu@zju.edu.cn>
2017-05-22 14:42:14 +0800

    调整main结构，用于最终展示

--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,57 @@
 
 #include "path_tracer.hpp"
 
-crt::Scene DummyScene() {
-  crt::Triangle triangle;
-  triangle.normal = make_float3(0, 0, -1);
-  triangle.vertices[0] = make_float3(1, 1, 1);
-  triangle.vertices[1] = make_float3(-1, 1, 1);
-  triangle.vertices[2] = make_float3(1, -0.5, 1);
-  triangle.material.emitted_color = make_float3(0.78, 0, 0.78);
+const float kAperatureRadius = 0.04;
+const uint2 kResolution = make_uint2(512, 512);
+const size_t kNumScenes = 2;
 
-  crt::Scene scene;
-  scene.triangles.push_back(triangle);
-  return scene;
-}
+const std::string kSceneFiles[kNumScenes]{
+    "../scenes/scene-1/scene01.obj", "../scenes/scene-2/scene02.obj",
+};
+const cupt::Camera kCameras[2]{
+    cupt::Camera(kResolution, make_float3(0, 5, 10), make_float3(0, 0, -1),
+                 make_float3(0, 1, 0), make_float2(90, 90), kAperatureRadius,
+                 5),
+    cupt::Camera(kResolution, make_float3(0, 5, 10), make_float3(0, 0, -1),
+                 make_float3(0, 1, 0), make_float2(90, 90), kAperatureRadius,
+                 5),
+};
 
-crt::Camera DummyCamera() {
-  uint2 resolution = make_uint2(1024, 1024);
-  float3 position = make_float3(0);
-  float3 view = make_float3(0, 0, 1);
-  float3 up = make_float3(0, 1, 0);
-  float2 fov = make_float2(90, 90);
-  float aperture_radius = 0.04;
-  float focal_distance = 4;
+int main(int argc, const char* argv[]) {
+  if (argc < 3) {
+    std::cout << "Usage: PathTracer SCENE-ID OUTPUT [SAMPLES] [DEPTH]"
+              << std::endl;
+    return -1;
+  }
 
-  return crt::Camera(resolution, position, view, up, fov, aperture_radius,
-                     focal_distance);
-}
-
-int main() {
   std::cout << "Initializing CUDA Runtime..." << std::flush;
   cudaSetDevice(0);
   std::cout << "Done" << std::endl;
 
-  crt::PathTracer::Parameter parameter;
-  parameter.max_trace_depth = 5;
-  parameter.mc_sample_times = 50;
-  crt::PathTracer tracer(parameter);
+  cupt::PathTracer::Parameter parameter;
+  parameter.mc_sample_times = argc > 3 ? atoi(argv[3]) : 1;
+  parameter.max_trace_depth = argc > 4 ? atoi(argv[4]) : 1;
+  cupt::PathTracer tracer(parameter);
 
-  tracer.SetScene(DummyScene());
+  const size_t id = atoi(argv[1]) - 1;
+  assert(id < kNumScenes);
 
-  std::cout << "Rendering" << std::endl;
-  crt::Image image = tracer.Render(DummyCamera());
+  const std::string& filename = kSceneFiles[id];
+  const std::string basedir(filename, 0, filename.rfind("/") + 1);
+
+  std::cout << "Loading Scene..." << std::flush;
+  cupt::Scene scene(filename.c_str(), basedir.c_str());
+  scene.light = 20;
+  tracer.SetScene(scene);
+  std::cout << "Done" << std::endl;
+
+  std::cout << "Rendering..." << std::flush;
+  const cupt::Camera& camera = kCameras[id];
+  cupt::Image image = tracer.Render(camera);
   std::cout << "Done" << ::std::endl;
 
   std::cout << "Saving Result..." << std::flush;
-  bool success = image.Save("./result.ppm");
+  bool success = image.Save(argv[2]);
   if (success) {
     std::cout << "Success!" << std::endl;
   } else {
